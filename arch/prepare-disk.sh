9f4238ca83b86718e9bb2d473dd2e1865d8d0b69
Andrés Peralta <ar.peralta.3@gmail.com>
2017-07-29 18:27:45 -0500

    Use functions on prepare disk script

--- arch/prepare-disk.sh
+++ arch/prepare-disk.sh
@@ -1,21 +1,38 @@
 #!/bin/bash
-set -e
 >prepare-disk.log
 >prepare-disk.err
 exec > >(tee -ia prepare-disk.log) 2> >(tee -ia prepare-disk.err >&2)
 
 disk_name=$1
 
-if test -n "$disk_name"; then
-  printf "\n\n======= PLEASE MAKE SURE TO ERASE AND UNMOUNT DISK /dev/${disk_name} FIRST ======\n\n"
-  
-  cryptsetup open --type plain "/dev/${disk_name}" cryptdisk --key-file /dev/random && echo "Mount disk /dev/${disk_name} with random encrypt"  
-  
-  dd if=/dev/zero of=/dev/mapper/cryptdisk status=progress bs=1M && echo "Wipe /dev/${disk_name}"
-  
-  cryptsetup close cryptdisk && echo "Close the temporary container"
-else
-  echo "Define disk name. Run lsblk -f to know the name of the disks" >&2
-fi
+function mount_cryptdisk {
+  cryptsetup open --type plain "/dev/${disk_name}" cryptdisk --key-file /dev/random
+}
 
-exit 0
+function wipe_cryptdisk {
+  dd if=/dev/zero of=/dev/mapper/cryptdisk status=progress bs=1M
+}
+
+function close_cryptdisk {
+  cryptsetup close cryptdisk
+  echo "Close the temporary container"
+}
+
+if test -n "$disk_name" ; then
+  echo "======= PLEASE MAKE SURE TO ERASE AND UNMOUNT DISK /dev/${disk_name} FIRST ======\n\n"
+  
+  if mount_cryptdisk ; then
+    echo "Mount disk /dev/${disk_name} with random encrypt"
+    if wipe_cryptdisk ; then
+      echo "Wipe /dev/${disk_name}"
+      close_cryptdisk
+    else
+      close_cryptdisk
+      exit
+    fi
+  else
+    exit
+  fi
+else
+  echo "Please define disk name. Run lsblk -f to know the name of the disks"
+fi
