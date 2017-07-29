7b2f338c86a91dde7b24bfcf48355d098847ce06
Andrés Peralta <ar.peralta.3@gmail.com>
2017-07-29 17:03:52 -0500

    Fix prepare disk script

--- arch/prepare-disk.sh
+++ arch/prepare-disk.sh
@@ -4,13 +4,13 @@
 disk_name=$1
 
 if test -n "$disk_name"; then
-  printf "\n\n======= PLEASE MAKE SURE TO ERASE DISK FIRST ======\n\n"
+  printf "\n\n======= PLEASE MAKE SURE TO ERASE AND UNMOUNT DISK /dev/${disk_name} FIRST ======\n\n"
 
   printf "\n\nMount disk /dev/${disk_name} with random encrypt\n\n"
   cryptsetup open --type plain "/dev/${disk_name}" cryptdisk --key-file /dev/random
 
   printf "\n\nWipe /dev/${disk_name}\n\n"
-  dd if=/dev/zero of=/dev/mapper/cryptdisk status=progress bs=1M
+  dd if=/dev/zero of=/dev/mapper/cryptdisk status= progress bs=1M
 
   printf "\n\nClose the temporary container\n\n"
   cryptsetup close cryptdisk
