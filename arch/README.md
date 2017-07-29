1dd79fd44de0276d713362e22215c1b40d822401
Andrés Peralta <ar.peralta.3@gmail.com>
2017-07-29 16:38:14 -0500

    Add readme to arch and better prepare-disk script

---
+++ arch/README.md
@@ -0,0 +1,18 @@
+## Overview
+
+These are scripts for my personals arch linux instllations.
+
+### Arch installation
+
++ Download configuration file:
+
+```
+$ curl -LOJ 
+https://raw.githubusercontent.com/arperalta3/scripts/master/arch/install-1.sh >> install-1.sh
+```
+
++ Run install
+
+ ```
+$ sh install-1.sh
+```
--- arch/prepare-disk.sh
+++ arch/prepare-disk.sh
@@ -1,10 +1,15 @@
 #! /bin/sh
 
+#name like /dev/sda
+disk_name=$1
+
+printf "\n\n======= PLEASE MAKE SURE TO ERASE DISK FIRST ======\n\n"
+
 printf "\n\nMount disk with random encrypt -> disk sda\n\n"
-cryptsetup open --type plain /dev/sda cryptdisk --key-file /dev/random
+cryptsetup open --type plain "${disk_name}" cryptdisk --key-file /dev/random
 
 printf "\n\nWipe disk\n\n"
 dd if=/dev/zero of=/dev/mapper/cryptdisk status=progress bs=1M
 
 printf "\n\nClose the temporary container\n\n"
-cryptsetup close cryptroot
+cryptsetup close cryptdisk
