a6c3b779c3da9fa55542a7e557a371bcee1799b6
Andrés Peralta <ar.peralta.3@gmail.com>
2017-07-29 17:53:45 -0500

    Stop prepare disk script if any error

--- arch/prepare-disk.sh
+++ arch/prepare-disk.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 >prepare-disk.log
 >prepare-disk.err
 exec > >(tee -ia prepare-disk.log) 2> >(tee -ia prepare-disk.err >&2)
@@ -7,15 +8,12 @@ disk_name=$1
 
 if test -n "$disk_name"; then
   printf "\n\n======= PLEASE MAKE SURE TO ERASE AND UNMOUNT DISK /dev/${disk_name} FIRST ======\n\n"
-
-  printf "\n\nMount disk /dev/${disk_name} with random encrypt\n\n"
-  cryptsetup open --type plain "/dev/${disk_name}" cryptdisk --key-file /dev/random
-
-  printf "\n\nWipe /dev/${disk_name}\n\n"
-  dd if=/dev/zero of=/dev/mapper/cryptdisk status=progress bs=1M
-
-  printf "\n\nClose the temporary container\n\n"
-  cryptsetup close cryptdisk
+  
+  cryptsetup open --type plain "/dev/${disk_name}" cryptdisk --key-file /dev/random && echo "Mount disk /dev/${disk_name} with random encrypt"  
+  
+  dd if=/dev/zero of=/dev/mapper/cryptdisk status=progress bs=1M && echo "Wipe /dev/${disk_name}"
+  
+  cryptsetup close cryptdisk && echo "Close the temporary container"
 else
-  echo "Define disk name. Run lsblk -f to know the name of the disks" >&2
+  echo "ERROR: Define disk name. Run lsblk -f to know the name of the disks" >&2
 fi
