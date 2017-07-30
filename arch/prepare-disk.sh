39e8a9523f00d2dfe85aacc8806043ff7432ac7d
Andrés Peralta <ar.peralta.3@gmail.com>
2017-07-30 15:29:34 -0500

    Change error manamengt prepare disk script

--- arch/prepare-disk.sh
+++ arch/prepare-disk.sh
@@ -1,47 +1,29 @@
 #!/bin/bash
+# Fail if variables unset
+set -o nounset
+# Fail if any error
+set -o errexit
+
+# Prepare logs
 >prepare-disk.log
 >prepare-disk.err
 exec > >(tee -ia prepare-disk.log) 2> >(tee -ia prepare-disk.err >&2)
 
+# Define variables
 disk_name=$1
+mapped_name=cryptdisk
 
-function mount_cryptdisk {
-  cryptsetup open --type plain "/dev/${disk_name}" cryptdisk --key-file /dev/random
+function close_mapped_disk {
+  cryptsetup close "${mapped_name}" && echo "====== OK: Close the temporary container ======"
 }
 
-function wipe_cryptdisk {
-  dd if=/dev/zero of=/dev/mapper/cryptdisk status=progress bs=1M
-}
+echo "====== INFO: To define disk name run lsblk -f to know the name of the disks ======"
 
-function close_cryptdisk {
-  cryptsetup close cryptdisk
-}
+echo "====== INFO: PLEASE MAKE SURE TO ERASE AND UNMOUNT DISK /dev/${disk_name} FIRST ======"
 
-if test -n "$disk_name" ; then
-  echo "======= PLEASE MAKE SURE TO ERASE AND UNMOUNT DISK /dev/${disk_name} FIRST ======"
-  
-  if mount_cryptdisk ; then
-    echo "====== OK: Mount disk /dev/${disk_name} with random encrypt ======="
-    if wipe_cryptdisk ; then
-      echo "====== OK: Wipe /dev/${disk_name} ====== "
-      if close_cryptdisk ; then
-        echo "====== OK: Close the temporary container ======"
-      else
-        exit 1
-      fi
-    else
-      if close_cryptdisk ; then
-        echo "====== OK: Close the temporary container ======"
-      else 
-        exit 1
-      fi
-      exit 1
-    fi
-  else
-    exit 1
-  fi
-else
-  echo "====== ERROR: Please define disk name. Run lsblk -f to know the name of the disks ======"
+if cryptsetup open --type plain "/dev/${disk_name}" "${mapped_name}" --key-file /dev/random && echo "====== OK: Mount disk /dev/${disk_name} with random encrypt ======="
+then
+  trap close_mapped_disk INT TERM EXIT
+  dd if=/dev/zero of="/dev/mapper/${mapped_name}" status=progress bs=1M && echo "====== OK: Wipe /dev/${disk_name} ======"
+  trap - INT TERM EXIT
 fi
-
-exit 0
